e66dcb5406815e89ff342e00683f0f98b893a76d
chenxin09 <chenxin09@kuaishou.com>
2021-04-14 10:32:06 +0800

    eureka-server & eureka-client module finished

--- cloud-eureka-server7001/src/main/java/com/codechase/cloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/codechase/cloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.codechase.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author chenxin09 <chenxin09@kuaishou.com>
- * Created on 2021-04-12
+ * Created on 2021-04-13
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/codechase/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/codechase/cloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.codechase.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chenxin09 <chenxin09@kuaishou.com>
  * Created on 2021-04-09
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
